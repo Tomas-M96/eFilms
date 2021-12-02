216145c06a2aea36b8cf63ad66fb3da7a2f301d6
Tomas-M96 <tomasmoore@outlook.com>
2021-12-02 19:19:47 +0000

    Added controllers

---
+++ eFilms/Controllers/StoresController.cs
@@ -0,0 +1,30 @@
+﻿using eFilms.Data;
+using eFilms.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eFilms.Controllers
+{
+    public class StoresController : Controller
+    {
+        //readonly Db context
+        private readonly ApplicationDbContext context;
+
+        //Constructor for the controller
+        public StoresController(ApplicationDbContext _context)
+        {
+            context = _context;
+        }
+
+        //async
+        public async Task<IActionResult> Index()
+        {
+            List<Store> data = await context.Stores.ToListAsync();
+            return View();
+        }
+    }
+}
